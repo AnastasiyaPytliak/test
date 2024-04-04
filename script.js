1d097a6a4ee571ad73e7da1a228b7bfe3defa015
Anastasia <nastya.pytliak@gmail.com>
2024-04-05 01:52:36 +0300

    math

--- script.js
+++ script.js
@@ -27,10 +27,6 @@ nextElementBtn.addEventListener("click", () => {
   if (blockElementBtns.indexOf(active) >= 1) {
     prevElementBtn.classList.remove("inactive");
   }
-  // Add active when every questions has a check
-  if (active.querySelectorAll(".test-question__option.active").length == "4") {
-    nextElementBtn.classList.remove("inactive");
-  }
 });
 
 // ------Transition to Previous Step
@@ -40,27 +36,42 @@ prevElementBtn.addEventListener("click", () => {
   if (blockElementBtns.indexOf(active) == 0) {
     prevElementBtn.classList.add("inactive");
   }
-  // Add active when every questions has a check
-  if (active.querySelectorAll(".test-question__option.active").length == "4") {
-    nextElementBtn.classList.remove("inactive");
-  }
+});
+
+// ----Sum
+document.querySelectorAll(".test-questions").forEach((questionBlock) => {
+  let totalPoints = 0;
+  
+  questionBlock.querySelectorAll(".test-question").forEach((question) => {
+    question.addEventListener("click", () => {
+      totalPoints = 0;
+      if (questionBlock.querySelector(".test-question__option.active")) {
+        questionBlock.querySelectorAll(".test-question__option.active").forEach((activeOption) => {
+            totalPoints += parseInt(activeOption.getAttribute("data-points"));
+          });
+        console.log(totalPoints);
+        questionBlock.querySelector('.points').innerHTML = `${totalPoints}/10`
+      } else {
+        totalPoints = 0;
+      }
+      if (totalPoints === 10 && questionBlock.querySelectorAll(".test-question__option.active").length == "4") {
+        nextElementBtn.classList.remove("inactive");
+      } else {
+        nextElementBtn.classList.add("inactive");
+      }
+    });
+  });
 });
 
 // ------Choosing Answer
 pointElementBtns.forEach((pointBtn) => {
   pointBtn.addEventListener("click", () => {
     // Add actie class on point btns
-    pointBtn.parentElement
-      .querySelectorAll(".test-question__option")
-      .forEach((i) => {
+    pointBtn.parentElement.querySelectorAll(".test-question__option").forEach((i) => {
         i.classList.remove("active");
       });
     pointBtn.classList.add("active");
     const active = document.querySelector(".test-questions.active");
-    // Check on every steps
-    if (active.querySelectorAll(".test-question__option.active").length == "4") {
-      nextElementBtn.classList.remove("inactive");
-    }
     // Check on last step
     if (active.querySelectorAll(".test-question__option.active").length == "4" &&
       blockElementBtns.indexOf(active) == 9) {
@@ -73,8 +84,9 @@ pointElementBtns.forEach((pointBtn) => {
 // --------Sumbit Results
 function handleGetResults() {
   const data = [];
-  let id = window.location.search
+  let id = window.location.search;
   // data.push({name: 'lead_id', value: `${id.slice(4, attr.length)}`})
+  data.push({name: "lead_id", value: `123456789`});
   const elements = document.querySelectorAll(".test-question__options");
   elements.forEach((element) => {
     const name = element.dataset.name;
@@ -84,35 +96,38 @@ function handleGetResults() {
       value: value,
     });
   });
-  data.push({name: 'lead_id', value: `123456789`})
- 
+
+
   const formData = data.reduce((obj, item) => {
     obj[item.name] = item.value;
     return obj;
   }, {});
 
   console.log(formData);
-  fetch('https://advaga.peaceful-mahavira.173-249-18-74.plesk.page/webhook/fd464835-baeb-4c84-b5e9-24dca078df81', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+  fetch(
+    "https://advaga.peaceful-mahavira.173-249-18-74.plesk.page/webhook/fd464835-baeb-4c84-b5e9-24dca078df81",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     }
-    return response.json();
-  })
-  .then(data => {
-    resultsBlockElement.innerHTML = `
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      resultsBlockElement.innerHTML = `
     <div class="results__title">${data.result_header}</div> 
     <div class="results__info">${data.result}</div> 
-    `
-  })
-  .catch(error => {
-    console.error('Error sending form data:', error);
-  });
+    `;
+    })
+    .catch((error) => {
+      console.error("Error sending form data:", error);
+    });
 }
 resultsBtnElement.addEventListener("click", handleGetResults);
